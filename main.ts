747339a9562bd62a019c060d1ee2729a42d306b2
Sergio <sa.perez2@uniandes.edu.co>
2024-10-20 23:37:58 -0500

    Taller2Typescript

--- main.ts
+++ main.ts
@@ -3,6 +3,11 @@ import { series } from './data.js';
 
 const seriesTbody: HTMLElement = document.getElementById('series')!;
 const avgSeasonsElm: HTMLElement = document.getElementById('average-seasons')!;
+const seriesName: HTMLElement = document.getElementById('seriesName')!;
+const seriesImg: HTMLElement = document.getElementById('serieImage')!;
+const seriesDescription: HTMLElement = document.getElementById('seriesDescription')!;
+const seriesLink: HTMLElement = document.getElementById('seriesLink')!;
+
 
 renderSeriesInTable(series);
 avgSeasonsElm.innerHTML = `${getAverageSeasons(series)}`
@@ -11,11 +16,42 @@ function renderSeriesInTable(series :Serie[]): void{
     series.forEach(serie =>{
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${serie.id}</td>
-                               <td>${serie.name}</td>
+                               <td><a href="#">${serie.name}</a></td>
                                <td>${serie.channel}</td>
                                <td>${serie.seasons}</td>`;
         seriesTbody.appendChild(trElement);
     });
+}
+
+seriesTbody.addEventListener('click', (event) => {
+event.preventDefault();
+const target = event.target as HTMLAnchorElement;
+if (target && target.tagName === 'A') {
+  const text = target.textContent || target.innerText;;
+  showSeriesinCard(text)
+}
+});
+
+function showSeriesinCard (text: string){
+  let serieFound: Serie | undefined  = searchSeriesByName(text, series);
+  
+  if (serieFound) {
+    seriesName.textContent = `${serieFound.name}`;
+    seriesImg.innerHTML = `<img src = ${serieFound.photo}`;
+    seriesDescription.textContent = `${serieFound.description}`;
+    seriesLink.innerHTML = `<a href = ${serieFound.url}>${serieFound.url}</a>`
+  } else {
+    seriesName.textContent = 'Serie no encontrada';
+  }
+}
+
+function searchSeriesByName(nameKey: string, series: Serie[]) : Serie | undefined{
+  for (var i = 0; i < series.length; i++) {
+    if (series[i].name === nameKey) {
+      return series[i];
+    }
+  }
+  return undefined;
 }
 
 function getAverageSeasons (series: Serie[]): number{
@@ -25,4 +61,13 @@ function getAverageSeasons (series: Serie[]): number{
                        numberSeries = numberSeries + 1;
     });
     return totalSeasons/numberSeries;
+}
+
+function clearCoursesInTable() {
+    while (seriesTbody.hasChildNodes()) {
+      if (seriesTbody.firstChild != null) {
+        seriesTbody.removeChild(seriesTbody.firstChild);
+       
+      }
+    }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,47 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
 var avgSeasonsElm = document.getElementById('average-seasons');
+var seriesName = document.getElementById('seriesName');
+var seriesImg = document.getElementById('serieImage');
+var seriesDescription = document.getElementById('seriesDescription');
+var seriesLink = document.getElementById('seriesLink');
 renderSeriesInTable(series);
 avgSeasonsElm.innerHTML = "".concat(getAverageSeasons(series));
 function renderSeriesInTable(series) {
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                               <td>").concat(serie.name, "</td>\n                               <td>").concat(serie.channel, "</td>\n                               <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                               <td><a href=\"#\">").concat(serie.name, "</a></td>\n                               <td>").concat(serie.channel, "</td>\n                               <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
+}
+seriesTbody.addEventListener('click', function (event) {
+    event.preventDefault();
+    var target = event.target;
+    if (target && target.tagName === 'A') {
+        var text = target.textContent || target.innerText;
+        ;
+        showSeriesinCard(text);
+    }
+});
+function showSeriesinCard(text) {
+    var serieFound = searchSeriesByName(text, series);
+    if (serieFound) {
+        seriesName.textContent = "".concat(serieFound.name);
+        seriesImg.innerHTML = "<img src = ".concat(serieFound.photo);
+        seriesDescription.textContent = "".concat(serieFound.description);
+        seriesLink.innerHTML = "<a href = ".concat(serieFound.url, ">").concat(serieFound.url, "</a>");
+    }
+    else {
+        seriesName.textContent = 'Serie no encontrada';
+    }
+}
+function searchSeriesByName(nameKey, series) {
+    for (var i = 0; i < series.length; i++) {
+        if (series[i].name === nameKey) {
+            return series[i];
+        }
+    }
+    return undefined;
 }
 function getAverageSeasons(series) {
     var totalSeasons = 0;
@@ -18,4 +51,11 @@ function getAverageSeasons(series) {
         numberSeries = numberSeries + 1;
     });
     return totalSeasons / numberSeries;
+}
+function clearCoursesInTable() {
+    while (seriesTbody.hasChildNodes()) {
+        if (seriesTbody.firstChild != null) {
+            seriesTbody.removeChild(seriesTbody.firstChild);
+        }
+    }
 }
